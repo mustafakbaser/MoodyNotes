f6dd2e3de0059120f1e665701473c93e53d33f3e
MustafaBaser <32432625+MustafaBaser@users.noreply.github.com>
2020-03-09 00:05:42 +0300

    Moody Notes

---
+++ app/src/main/java/com/mustafabaser/moodynotes/Splash.java
@@ -0,0 +1,33 @@
+package com.mustafabaser.moodynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import android.content.Intent;
+import android.os.Handler;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Splash extends AppCompatActivity {
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        },3000);
+    }
+}
---
+++ app/src/main/java/com/mustafabaser/moodynotes/authentication/Register.java
@@ -0,0 +1,16 @@
+package com.mustafabaser.moodynotes.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mustafabaser.moodynotes.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
