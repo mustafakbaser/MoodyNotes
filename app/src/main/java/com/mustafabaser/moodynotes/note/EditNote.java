6b924416a1fb97519db59e7d2bc77e123e071db9
Mustafa <mkursatbaser@gmail.com>
2021-10-19 01:01:28 +0300

    Constraint Update - 2.0

--- app/src/main/java/com/mustafabaser/moodynotes/note/EditNote.java
+++ app/src/main/java/com/mustafabaser/moodynotes/note/EditNote.java
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
